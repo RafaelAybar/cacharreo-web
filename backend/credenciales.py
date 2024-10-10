7c374ba026181dfa36c8456c8d31d71c8a283114
Rafael Aybar <rafaelaybar@hotmail.com>
2024-10-11 00:32:28 +0200

    Solventada la comparació  de hashes

--- backend/credenciales.py
+++ backend/credenciales.py
@@ -3,10 +3,11 @@ from os import urandom
 
 def hash_contrasena(contrasena: str):
     sal = urandom(25)
-    hash_contrasena = scrypt(contrasena.encode(), salt=sal, n=128, r=8, p=1)
+    hash_contrasena = scrypt(contrasena.encode(), salt=sal, n=16384, r=8, p=1)
     return sal + hash_contrasena
 
 def comprobar_contrasena(contrasena: str, hash_almacenado: bytes) -> bool:
     sal_almacenada = hash_almacenado[:25]
-    nuevo_hash = scrypt(contrasena.encode(), salt=sal_almacenada, n=128, r=8, p=1)
-    return hash_almacenado == nuevo_hash
+    clave_almacenada = hash_almacenado[25:]
+    nuevo_hash = scrypt(contrasena.encode(), salt=sal_almacenada, n=16384, r=8, p=1)
+    return nuevo_hash == clave_almacenada
